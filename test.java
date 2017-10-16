ceaacff78a3dd90e43e801898309837129fd628d
tanvir-ux <tanviralamshuvo@gmail.com>
2017-10-15 19:49:41 -0700

    java class

--- test.java
+++ test.java
@@ -1,11 +1,22 @@
-public abstract void Character{
-int x,y;
+public abstract void Bird{
+	int x,y;
+	double move,height;
 
-public static final int Width = 4
+	public static final int Width = 700;
+	public static final int height = 300;
 
-public void doSomething(){
+	public static void doSomething(){
+
+
+		debug.log("I am doing something ");
+
+	}
+
+
 
 
 }
 
-}
+Bird bd = new Bird();
+
+bd.doSomething();
